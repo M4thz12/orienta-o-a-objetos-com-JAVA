3a0ec5c265a99d0d94ed7cc467a646b7a8ee875d
m4thz12 <rm98832@fiap.com.br>
2024-02-05 11:42:02 -0300

    iniciando

---
+++ aula-01/aula01.java
@@ -0,0 +1,49 @@
+public class teste1{
+	public static void main(String [] args){
+		double nota1 = 8.4, nota2 = 7.5, nota3 = 9.5, notaFinal;
+		String nome =  "matheus";
+		notaFinal = ((nota1 + nota2 + nota3)/3);
+
+		System.out.println("o aluno" + nome + "teve a media final de " + notaFinal);
+	}
+
+
+}
+-=-=-=-=-=-=-=-=-=-=-
+public class teste2{
+	public static void main(String [] args){
+		double valor = 256, p10valor;
+		p10valor = valor/10;
+
+	
+		System.out.println("10% de 256 é: " + p10valor);
+	}
+
+}
+-=-=-=-=-=-=-=-=-=-=-
+public class teste3{
+	public static void main(String [] args){
+
+	double x = 8, y = 4, sm, mlt, div, sub;
+	sm = 8 + 4;
+	mlt = 8 * 4;
+	div = 8 /4;
+	sub = 8 - 4;
+
+	System.out.println(sm);
+	System.out.println(mlt);
+	System.out.println(div);
+	System.out.println(sub);
+	}
+}
+-=-=-=-=-=-=-=-=-=-
+public class teste4{
+	public static void main(String [] args){
+	double cinco = 5;
+	for(int i = 0; i < 10; i++){
+	System.out.println(cinco * i);
+
+		}
+	
+	}	
+}
